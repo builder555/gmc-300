18e49f169557b745805a6981041f98805c85818e
builder555 <andreixk@gmail.com>
2022-06-26 13:44:47 -0400

    ensure port detector can find usb on RPi, added delay when reading for the first time

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import glob
+from time import sleep
 import serial
+
+def can_open_port(port):
+    try:
+        with serial.Serial(port, 57600, timeout=1):
+            print(f'port detected: {port}')
+        return True
+    except:
+        return False
 
 def detect_port():
     print('detecting port...')
-    ports = glob.glob('/dev/*usb*')
+    ports = glob.glob('/dev/*usb*') + glob.glob('/dev/*USB*')
     for port in ports:
-        try:
-            with serial.Serial(port, 57600, timeout=1):
-                print(f'port detected: {port}')
+        if can_open_port(port):
             return port
-        except serial.SerialException:
-            pass
     raise Exception('no USB serial port detected, please ensure the device is connected')
 
 def get_version(sr):
@@ -30,19 +35,27 @@ def get_serial(sr):
     return  ''.join([hex(x)[2:] for x in serial]).upper()
 
 def power_off(sr):
+    print('powering off...')
     sr.write("<POWEROFF>>".encode())
 
 def power_on(sr):
+    print('powering on...')
     sr.write("<POWERON>>".encode())
 
-def test(sr):
-    sr.write("<GETGYRO>>".encode())
-    resp = sr.readline()
-    return resp
+def visual_delay(seconds):
+    for i in range(seconds, 0, -1):
+        print(i, end=' '*len(str(i)))
+        sleep(1)
+        print('\r', end='')
 
 if __name__ == '__main__':
     port = detect_port()
     with serial.Serial(port, 57600, timeout=2) as s:
+        cpm = get_cpm(s)
+        if cpm == 0:
+            power_on(s)
+            print('please wait a few seconds to get a stable reading')
+            visual_delay(40)
         print('version:', get_version(s))
         print('serial:', get_serial(s))
         print('CPM:', get_cpm(s))
--- readme.md
+++ readme.md
@@ -2,18 +2,23 @@
 
 Python interface for a GQ GMC-300, GMC-300E PLUS, GMC-320 geiger counters.
 
+For the most up to date information refer to the original GQ [website](http://www.gqelectronicsllc.com/comersus/store/download.asp)
+
 ### Installation
-```
+```bash
 pipenv install
+# or if you prefer `.venv` to be in the same directory:
+PIPENV_VENV_IN_PROJECT=1 pipenv install
 ```
 
+
 ### Run
-```
+```bash
 pipenv run start
 ```
 
 ### Example output:
-```
+```bash
 detecting port...
 port detected: /dev/cu.usbserial-210
 version: GMC-300Re 4.22
